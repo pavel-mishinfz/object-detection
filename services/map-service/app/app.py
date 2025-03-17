98d668b41e99a1d04445cf70e580485d86021b7a
Pavel Mishin <pavel.mishinfz@gmail.com>
2025-03-17 14:53:34 +0300

    Add routes and methods for CRUD-operations with Area
    1) GET '/areas' and GET '/areas/{id}'
    2) PUT '/areas/{id}'
    3) DELETE '/areas/{id}'

--- services/map-service/app/app.py
+++ services/map-service/app/app.py
@@ -1,9 +1,11 @@
+import uuid
+
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .database import DB_INITIALIZER, get_async_session
 
-from .schemas import Area, AreaIn
+from .schemas import Area, AreaSummary, AreaIn, AreaUpdate
 
 from . import crud
 from . import config
@@ -13,12 +15,81 @@ cfg: config.Config = config.load_config()
 
 app = FastAPI(title='Map Service')
 
-@app.post('/areas', response_model=Area, summary='Добавляет полигон в базу', tags=["areas"])
+
+@app.post(
+    '/areas',
+    response_model=Area,
+    summary='Добавляет полигон в базу',
+    tags=["areas"]
+)
 async def add_area(
         area_in: AreaIn,
         db: AsyncSession = Depends(get_async_session)
 ):
     return await crud.create_area(db, area_in)
+
+
+@app.get(
+    '/areas/{area_id}',
+    response_model=Area,
+    summary='Возвращает информацию о полигоне',
+    tags=['areas']
+)
+async def get_area(
+        area_id: uuid.UUID,
+        db: AsyncSession = Depends(get_async_session)
+):
+    area = await crud.get_area(db, area_id)
+    if area is None:
+        raise HTTPException(status_code=404, detail="Полигон не найден")
+    return area
+
+
+@app.get(
+    '/areas',
+    response_model=list[AreaSummary],
+    summary='Возвращает список полигонов пользователя',
+    tags=['areas']
+)
+async def get_list_areas(
+        user_id: uuid.UUID,
+        db: AsyncSession = Depends(get_async_session)
+):
+    return await crud.get_list_areas(db, user_id)
+
+
+@app.put(
+    '/areas/{area_id}',
+    response_model=Area,
+    summary='Обновляет полигон',
+    tags=['areas']
+)
+async def update_area(
+        area_id: uuid.UUID,
+        area_update: AreaUpdate,
+        db: AsyncSession = Depends(get_async_session)
+):
+    area = await crud.update_area(db, area_id, area_update)
+    if area is None:
+        raise HTTPException(status_code=404, detail="Полигон не найден")
+    return area
+
+
+@app.delete(
+    '/areas/{area_id}',
+    response_model=Area,
+    summary='Удаляет полигон',
+    tags=['areas']
+)
+async def delete_area(
+        area_id: uuid.UUID,
+        db: AsyncSession = Depends(get_async_session)
+):
+    area = await crud.delete_area(db, area_id)
+    if area is None:
+        raise HTTPException(status_code=404, detail="Полигон не найден")
+    return area
+
 
 @app.on_event("startup")
 async def on_startup():
--- services/map-service/app/crud.py
+++ services/map-service/app/crud.py
@@ -1,11 +1,10 @@
 import uuid
-from datetime import datetime, timezone
 
 from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .database import models
-from .schemas import AreaIn
+from .schemas import AreaIn, AreaUpdate
 
 from geoalchemy2.shape import from_shape
 from shapely.geometry import shape
@@ -33,7 +32,9 @@ async def create_area(
     db.add(db_area)
     await db.commit()
     await db.refresh(db_area)
+
     return db_area
+
 
 async def get_area(
     db: AsyncSession, area_id: uuid.UUID
@@ -41,4 +42,67 @@ async def get_area(
     """
     Возвращает информацию о полигоне
     """
-    pass
+    result = await db.execute(select(models.Area) \
+                              .filter(models.Area.id == area_id)
+                              )
+    return result.scalars().one_or_none()
+
+
+async def get_list_areas(
+    db: AsyncSession, user_id: uuid.UUID, skip: int = 0, limit: int = 100
+) -> list[models.Area]:
+    """
+    Возвращает список полигонов текущего пользователя
+    """
+    result = await db.execute(select(models.Area) \
+                              .filter(models.Area.user_id == user_id) \
+                              .order_by(models.Area.created_at.desc()) \
+                              .offset(skip) \
+                              .limit(limit)
+                              )
+    return result.scalars().all()
+
+
+async def update_area(
+        db: AsyncSession, area_id: uuid.UUID, area_update: AreaUpdate
+    ) -> models.Area | None:
+    """
+    Обновляет информацию о полигоне
+    """
+
+    # Преобразуем GeoJSON в WKBElement
+    shapely_geometry = shape(area_update.geometry.model_dump())
+    db_geometry = from_shape(shapely_geometry, srid=4326)
+    area_update.geometry = db_geometry
+
+    data_to_update = {
+        'name': area_update.name,
+        'geometry': area_update.geometry
+    }
+
+    result = await db.execute(update(models.Area) \
+                              .where(models.Area.id == area_id) \
+                              .values(data_to_update)
+                              )
+    await db.commit()
+
+    if result:
+        return await get_area(db, area_id)
+    return None
+
+
+async def delete_area(
+        db: AsyncSession, area_id: uuid.UUID
+) -> models.Area | None:
+    """
+    Удаляет информацию о полигоне
+    """
+    deleted_area = await get_area(db, area_id)
+    await db.execute(delete(models.Area) \
+                     .filter(models.Area.id == area_id)
+                     )
+
+    await db.commit()
+
+    return deleted_area
+
