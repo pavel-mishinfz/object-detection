352e78a8ca298fa409a8d2555357883777dff98c
Pavel Mishin <pavel.mishinfz@gmail.com>
2025-05-18 01:45:21 +0300

    Add get_analysis_results CRUD-method

--- services/analysis-service/app/crud/analysis.py
+++ services/analysis-service/app/crud/analysis.py
@@ -1,5 +1,6 @@
 import uuid
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..database import models
@@ -8,13 +9,14 @@ from geoalchemy2 import WKBElement
 
 
 async def create_analysis(
-        db: AsyncSession, image_id: uuid.UUID, geometry: WKBElement, object_type_id: int
+        db: AsyncSession, polygon_id: uuid.UUID, image_id: uuid.UUID, geometry: WKBElement, object_type_id: int
     ) -> models.SegmentationResult | None:
     """
     Сохраняет информацию о результате сегментирования снимка
     """
 
     db_analysis = models.SegmentationResult(
+        polygon_id=polygon_id,
         image_id=image_id,
         geometry=geometry,
         object_type_id=object_type_id
@@ -25,3 +27,17 @@ async def create_analysis(
     await db.refresh(db_analysis)
 
     return db_analysis
+
+
+async def get_analysis_results(
+    db: AsyncSession, polygon_id: uuid.UUID, skip: int = 0, limit: int = 100
+) -> list[models.SegmentationResult]:
+    """
+    Возвращает список результатов анализа определенного полигона
+    """
+    result = await db.execute(select(models.SegmentationResult) \
+                              .filter(models.SegmentationResult.polygon_id == polygon_id) \
+                              .offset(skip) \
+                              .limit(limit)
+                              )
+    return result.scalars().all()
